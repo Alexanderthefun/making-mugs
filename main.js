f523e6c629afb2ad23171e21218a243c96eb91b8
Alex Fun <alexanderthefun91@gmail.com>
2022-12-12 10:27:38 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+
+const clay = [ "Clay", "Clay", "Clay", "Clay" ]
+const toFireInKiln = []
+
+for (const piece of clay) {
+   const newMug = `${piece} coffee mug`
+   toFireInKiln.push(newMug)
+}
+
+console.log(toFireInKiln) 
